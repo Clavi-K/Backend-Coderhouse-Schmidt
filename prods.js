0d91941d447a563aa093f52c5039f9eadf972bc5
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-03-03 22:47:57 -0300

    Entregable clase 16

--- prods.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { id: 1, name: "Cohete Thor", price: 20, thumbnail: "https://cdn1.iconfinder.com/data/icons/space-flat-galaxy-radio/512/starship-256.png" },
-    { id: 2, name: "Satelite Zeus", price: 100, thumbnail: "https://cdn1.iconfinder.com/data/icons/space-flat-galaxy-radio/512/satellite-256.png" },
-    { id: 3, name: "Cohete? Asteroid", price: 35, thumbnail: "https://cdn1.iconfinder.com/data/icons/space-flat-galaxy-radio/512/laser_gun-256.png" }
-];
